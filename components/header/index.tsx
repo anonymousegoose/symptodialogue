dfe373a056b62cdcd2a03f5609ccb7ec4064140c
TheErrorMaster <61210645+TheErrorMaster@users.noreply.github.com>
2023-06-23 11:39:45 -0700

    Update header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,13 +2,18 @@ import React from "react";
 
 interface HeaderChatProps {
     title: string;
+    onPress?: () => void;
+    onPress2?: () => void;
+    color?: string;
+    button?: boolean;
 }
 
 export const HeaderChat = (props: HeaderChatProps) => {
-    const { title } = props || {};
+    const { title, onPress, color = '#d0d0d5', onPress2, button = false } = props || {};
   return (
-    <div style={{ display: 'flex', height: 50, width: '100%', background: '#d0d0d5', alignItems: 'center', justifyContent: 'center'}}>
+    <div style={{ display: 'flex', height: 50, width: '100%', background: color, alignItems: 'center', justifyContent: 'center', borderRadius: button ? 40 : 0, margin: button ? 20 : 0}} onClick={() => onPress ? onPress() : null}>
         <div>{title}</div>
+        {onPress2 ? (<div style={{ marginLeft: 10, border: '1px solid black', borderRadius: 40, padding: 10}} onClick={() => onPress2()}>save chat</div>) : null}
     </div>
   );
 };
